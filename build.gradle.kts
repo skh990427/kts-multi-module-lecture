a929b4c8aab664bd76e13891e5a59370cabf8f48
송광호 <skh990427@naver.com>
2026-08-08 17:34:41 +0900

    feat :: 초기 설정 프로젝트 커밋

---
+++ build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    id("org.springframework.boot") version "3.2.3" apply false
+    id("io.spring.dependency-management") version "1.1.4" apply false
+    kotlin("jvm") version "2.0.21" apply false
+    kotlin("plugin.spring") version "2.0.21" apply false
+    kotlin("plugin.jpa") version "2.0.21" apply false
+}
+
+allprojects {
+    group = "org.ktor_lecture"
+    version = "1.0-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+    }
+}
+
+subprojects {
+    apply(plugin = "kotlin")
+    apply(plugin = "io.spring.dependency-management")
+
+    if (name == "bank-api") {
+        apply(plugin = "org.springframework.boot")
+        apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+        apply(plugin = "org.jetbrains.kotlin.plugin.jpa")
+    }
+
+    if (name == "bank-core") {
+        apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    }
+
+    if (name == "bank-domain") {
+        apply(plugin = "org.jetbrains.kotlin.plugin.jpa")
+    }
+
+    if (name == "bank-event") {
+        apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    }
+
+    the<io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension>().apply {
+        imports {
+            mavenBom("org.springframework.boot:spring-boot-dependencies:3.2.3")
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            jvmTarget = "17"
+            freeCompilerArgs += "-Xjsr305=strict"
+        }
+    }
+}
