8e30d87cb2ed8eeb04dfc23919f5f139e3caf107
송광호 <skh990427@naver.com>
2026-08-10 00:59:33 +0900

    feat :: 비동기 전송을 위한 Thread 관리

--- bank-event/build.gradle.kts
+++ bank-event/build.gradle.kts
@@ -1,4 +1,7 @@
 dependencies {
+    // slf4j
+    implementation("ch.qos.logback:logback-classic:1.4.14")
+
     implementation(project(":bank-domain"))
 
     implementation("org.springframework.boot:spring-boot-starter")
--- bank-event/src/main/kotlin/publisher/EventPublisher.kt
+++ bank-event/src/main/kotlin/publisher/EventPublisher.kt
@@ -1,7 +1,7 @@
 package com.lecture.bank.event.publisher
 
 import com.lecture.bank.domain.event.DomainEvent
-import com.sun.org.slf4j.internal.LoggerFactory
+import org.slf4j.LoggerFactory
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
@@ -22,12 +22,24 @@ class EventPublisherImpl(
     private val logger = LoggerFactory.getLogger(EventPublisherImpl::class.java)
 
     override fun publish(event: DomainEvent) {
-        eventPublisher.publishEvent(event)
+        logger.info("Publishing event: $event")
+        try {
+            eventPublisher.publishEvent(event)
+            // TODO metrics
+        } catch (e : Exception) {
+            logger.error("Error publishing event!", e)
+        }
     }
 
     @Async("taskExecutor")
     override fun publishAsync(event: DomainEvent) {
-        eventPublisher.publishEvent(event)
+        logger.info("Publishing event: $event")
+        try {
+            eventPublisher.publishEvent(event)
+            // TODO metrics
+        } catch (e : Exception) {
+            logger.error("Error publishing event!", e)
+        }
     }
 
     override fun publishAll(events : List<DomainEvent>) {
